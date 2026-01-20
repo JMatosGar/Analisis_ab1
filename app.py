195069c4ec9012e1e4ee78fbd96354a8e76b4fdd
JMatosGar <javiermatosgarcia7@gmail.com>
2026-01-20 10:50:24 +0000

    Create app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Analisis de ab1",
+    layout="wide"
+)
+
+st.title("Preprocesado de ficheros ab1")
+st.write("Aplicación para el análisis de secuencias Sanger (.ab1)")
+
+carga_zip = st.file_uploader(
+    "Carga el archivo ZIP que contiene los ficheros .ab1",
+    type=["zip"]
+)
+
+if uploaded_zip is not None:
+    st.success(f"Archivo cargado: {uploaded_zip.name}")
