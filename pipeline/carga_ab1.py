b83093b04c4bdc775c6b732b7be18859ae1d4b75
JMatosGar <javiermatosgarcia7@gmail.com>
2026-01-20 12:43:05 +0000

    Create carga_ab1.py

---
+++ pipeline/carga_ab1.py
@@ -0,0 +1,58 @@
+from Bio import SeqIO
+import pandas as pd
+import statistics
+import os
+
+def cargar_ab1_zip(filepaths, umbral=20):
+
+  datos = []
+
+  if not filepaths:
+    raise ValueError("No se han proporcionado ficheros ab1")
+
+  for filepath in filepaths:
+    try:
+      record = SeqIO.read(filepath, "abi")
+    except Exception as e:
+      print(f"⚠️No se ha podido leer{filepath}: {e}")
+
+    secuencia = str(record.seq)
+    sec_length = len(secuencia)
+    calidad = record.letter_annotations.get("phred_quality", [])
+
+#Media y mediana de Phred.
+    calidad_media = statistics.mean(calidad) if calidad else 0
+    calidad_mediana = statistics.median(calidad) if calidad else 0
+
+#% bases con Phred >= umbral
+    calidad_alta = sum(1 for q in calidad if q >= umbral)
+    pct_calidad_alta = (calidad_alta / sec_length * 100) if sec_length > 0 else 0
+
+#nº de bases ambiguas (N)
+    num_amb = secuencia.upper().count("N")
+
+#Tramo continuo con bases de alta calidad más largo.
+    larga_calidad = 0
+    actual = 0
+    for q in calidad:
+      if q >= 20:
+        actual += 1
+        larga_calidad = max(larga_calidad, actual)
+      else:
+        actual = 0
+
+    datos.append({
+      "File Name": os.path.basename(filepath),
+      "Fasta Sequence": secuencia,
+      "Sequence Length": sec_length,
+      "Quality Scores": calidad,
+      "Mean Quality": calidad_media,
+      "Median Quality": calidad_mediana,
+      "% High-Quality Bases": pct_calidad_alta,
+      "Ambiguous Bases (N)": num_amb,
+      "Longest High-Quality Stretch": larga_calidad})
+    
+  if not datos:
+    raise ValueError("No se han encontrado ficheros ab1 válidos")
+      
+  return pd.DataFrame(datos)
