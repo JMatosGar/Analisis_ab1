388dc3ebdf78565cbef18b54a01bf6a055880eb7
JMatosGar <javiermatosgarcia7@gmail.com>
2026-01-22 14:41:25 +0000

    Update app.py

--- app.py
+++ app.py
@@ -205,22 +205,18 @@ if fasta_path and st.button("🔬 Alineamiento contra NCBI"):
             try:
                 blast_df = taxonomia(fasta_path, email)
                 st.session_state["blast_df"] = blast_df
-                st.success("✅ Se ha realizado el BLAST correctamente")                    
+                st.success("✅ Se ha realizado el BLAST correctamente") 
+                st.dataframe(st.session_state["blast_df"])
+
+                if  "blast_df" in st.session_state and not st.session_state["blast_df"].empty:
+                output_blast = BytesIO()
+                with pd.ExcelWriter(output_blast, engine='openpyxl') as writer:
+                    st.session_state["blast_df"].to_excel(writer, index=False, sheet_name="Trimmed Results")
+                processed_blast = output_blast.getvalue()
+            
+                st.download_button(label="📥 Descargar resultados de BLAST",
+                                   data=processed_blast, file_name=f"Resultado_blast_{zip_name}.xlsx",
+                                   mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
             
             except Exception as e:
-                st.error(f"❌ Error al ejecutar BLAST: {e}")        
-
-if  "blast_df" in st.session_state and not st.session_state["blast_df"].empty:
-    mostrar_blast = st.checkbox("📋 Mostrar resultado de BLAST contra NCBI")
-    
-    if mostrar_blast:
-        st.dataframe(st.session_state["blast_df"])
-        
-        output_blast = BytesIO()
-        with pd.ExcelWriter(output_blast, engine='openpyxl') as writer:
-            st.session_state["blast_df"].to_excel(writer, index=False, sheet_name="Trimmed Results")
-        processed_blast = output_blast.getvalue()
-            
-        st.download_button(label="📥 Descargar resultados de BLAST",
-                            data=processed_blast, file_name=f"Resultado_blast_{zip_name}.xlsx",
-                            mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+                st.error(f"❌ Error al ejecutar BLAST: {e}")    
